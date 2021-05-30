4e6eb859805222e243d8f69345d2f418a47ad3c7
estergrace <85030725+estergrace@users.noreply.github.com>
2021-05-30 15:09:32 +0800

    Add files via upload
    Name: (Anything)
    Email: user@cvsu.com
    Password: user

---
+++ App.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import LoginForm from './components/LoginForm';
+
+function App() {
+const adminUser = {
+  email: "user@cvsu.com",
+  password: "user"
+}
+
+const [user, setUser] = useState({name: "", email: ""});
+const [error, setError] = useState("");
+
+const Login = details => {
+  console.log(details);
+
+  if (details.email == adminUser.email && details.password == adminUser.password) {
+    console.log("Logged in");
+    setUser({
+      name: details.name,
+      email: details.email
+    });
+  } else {
+    console.log("Invalid User!");
+    setError("Invalid User!");
+  }
+}
+
+
+const Logout = () => {
+  setUser({ name: "", email: "" });
+}
+
+  return (
+    <div className="App">
+      {(user.email != "") ? (
+        <div className="Welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error}/>
+
+      )}
+    </div>
+  );
+}
+
+export default App;
